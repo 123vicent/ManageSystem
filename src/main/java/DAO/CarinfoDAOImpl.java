7add4ce15ce465a0845c7e1fa21f6f7216a00b43
ericho <473376490@qq.com>
2018-07-07 16:41:20 +0800

    Carinfo实体类及车辆信息查询DAO层

---
+++ src/main/java/DAO/CarinfoDAOImpl.java
@@ -0,0 +1,42 @@
+package DAO;
+
+import basic.Carinfo;
+import dbcon.DBConnect;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarinfoDAOImpl implements CarinfoDAO{
+    DBConnect dbc = new DBConnect();
+    Connection conn = dbc.getConnection();
+
+    public List<Carinfo> findAllByShopId(String shopuser_id){
+        List<Carinfo> carinfoList = new ArrayList<Carinfo>();
+        String sql = "select * from carinfo where shopuser_id=?";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1,shopuser_id);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Carinfo carinfo = new Carinfo();
+                carinfo.setShopuser_id(rs.getString(1));
+                carinfo.setBrand(rs.getString(2));
+                carinfo.setModel(rs.getString(3));
+                carinfo.setStock(rs.getInt(4));
+                carinfo.setPrice(rs.getDouble(5));
+                carinfo.setPic_url(rs.getString(6));
+
+                carinfoList.add(carinfo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  carinfoList;
+    }
+}
--- src/main/test/Main.java
+++ src/main/test/Main.java
@@ -1,7 +1,9 @@
 package test;
 
+import DAO.CarinfoDAO;
 import DAO.DAOFactory;
 import DAO.ShopaptDAO;
+import basic.Carinfo;
 import basic.Shopapt;
 import model.Shopuser;
 
@@ -10,6 +12,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String []args){
+        //test shopaptDAO
         ShopaptDAO shopaptdao = DAOFactory.getShopaptDAO();
         List<Shopapt> shopaptList = shopaptdao.findAllByShopId("s1");
         System.out.print("车店id ");
@@ -29,7 +32,28 @@ public class Main {
             System.out.print(shopaptList.get(i).getAp_state()+" ");
             System.out.print(shopaptList.size());
             System.out.println();
+        }
 
+        //test carinfoDAO
+        CarinfoDAO carinfoDAO = DAOFactory.getCarinfoDAO();
+        List<Carinfo> carinfoList = carinfoDAO.findAllByShopId("s1");
+        System.out.print("车店id ");
+        System.out.print("品牌 ");
+        System.out.print("型号 ");
+        System.out.print("库存 ");
+        System.out.print("价格 ");
+        System.out.print("图片 ");
+        System.out.println();
+
+        for(int i = 0; i<carinfoList.size();i++){
+            System.out.print(carinfoList.get(i).getShopuser_id()+" ");
+            System.out.print(carinfoList.get(i).getBrand()+" ");
+            System.out.print(carinfoList.get(i).getModel()+" ");
+            System.out.print(carinfoList.get(i).getStock()+" ");
+            System.out.print(carinfoList.get(i).getPrice()+" ");
+            System.out.print(carinfoList.get(i).getPic_url()+" ");
+            System.out.print(shopaptList.size());
+            System.out.println();
         }
     }
 }
