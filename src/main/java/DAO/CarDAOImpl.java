0ca503b61207dd62f818314e423280b8275ecac6
ericho <473376490@qq.com>
2018-07-08 11:17:21 +0800

    新增根据brand查询model方法findModelByBrand()

--- src/main/java/DAO/CarDAOImpl.java
+++ src/main/java/DAO/CarDAOImpl.java
@@ -96,6 +96,24 @@ public class CarDAOImpl implements CarDAO {
         return c;
     }
 
+    public List<String> findModelByBrand(String brand){
+        List<String> modelList = new ArrayList<String>();
+        String sql = "select model from car where brand=?";
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1,brand);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                modelList.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return modelList;
+    }
+
     public Car findByBrandModel(String brand,String model){
         Car c = new Car();
         String sql = "select * from car where brand=? and model=?";
