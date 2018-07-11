15d6782dd449d319242db66f906ffcbeca1271f2
ericho <473376490@qq.com>
2018-07-11 15:05:40 +0800

    修改

--- src/main/java/DAO/CarinfoDAO.java
+++ src/main/java/DAO/CarinfoDAO.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface CarinfoDAO {
     public List<Carinfo> findAllByShopId(String shopuser_id);
-    public List<Carinfo> findAllByTag(String shopuser_id,String brand,String model,String type);
+    public List<Carinfo> findAllByTag(String shopuser_id,String brand,String type);
 }
